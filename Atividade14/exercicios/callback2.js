6cb1fbe7c0627831437f25fb7cda19235bf5b3dd
LAUREN RODRIGUES OLIVEIRA <0030482411028@lti.fatecso.edu.br>
2025-11-24 10:18:39 -0300

    exercicios anteriores

--- Atividade14/exercicios/callback2.js
+++
@@ -1,9 +0,0 @@
-function exibeMensagensNaOrdem(mensagem, callback)
-{
-    console.log(mensagem);
-    callback();
-}
-
-exibeMensagensNaOrdem('essa é a primeira mensagem exibida na ordem', function(){
-    console.log('essa é a segunda mensagem exibida na ordem');
-});
